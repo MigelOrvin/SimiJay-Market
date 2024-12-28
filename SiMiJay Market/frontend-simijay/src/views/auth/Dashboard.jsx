015d79e4fdc800ef8defa50c1024938dc28ca8a7
Migel <migel8206@gmail.com>
2024-12-28 23:34:52 +0700

    fix login & register

--- SiMiJay Market/frontend-simijay/src/views/auth/Dashboard.jsx
+++ SiMiJay Market/frontend-simijay/src/views/auth/Dashboard.jsx
@@ -4,17 +4,19 @@ import Navbar from "../../components/Navbar";
 
 export default function Dashboard() {
   const [user, setUser] = useState([]);
+  const [successMessage, setSuccessMessage] = useState(""); // Add success message state
 
   useEffect(() => {
     const userData = localStorage.getItem("user");
 
     if (userData) {
       setUser(JSON.parse(userData));
+      setSuccessMessage("Berhasil Login"); // Set success message on login
     }
   }, []);
 
   return (
-      <>
+    <>
       <Navbar />
       <div className="container mt-5 mb-5">
         <div className="row">
@@ -25,6 +27,7 @@ export default function Dashboard() {
             <div className="card border-0 rounded shadow-sm">
               <div className="card-header">Dashboard</div>
               <div className="card-body">
+                {successMessage && <div className="alert alert-success">{successMessage}</div>} {/* Display success message */}
                 Selamat datang <strong>{user?.nama}</strong>, Anda login sebagai{" "}
                 <strong>{user?.role}</strong>
               </div>
